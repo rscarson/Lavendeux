9decbc6a473b56bca15469f06c80013241fc7f1f
Richard Carson <r.carson@calian.com>
2024-01-12 14:52:38 -0500

    fix casing

--- src-tauri/src/models/settings.rs
+++ src-tauri/src/models/settings.rs
@@ -14,10 +14,16 @@ pub struct Settings {
     pub language_code: String,
 }
 
+// Default enhanced_clipboard to true on all but linux
+#[cfg(target_os = "linux")]
+const DEFAULT_ENHANCED_CLIPBOARD: bool = false;
+#[cfg(not(target_os = "linux"))]
+const DEFAULT_ENHANCED_CLIPBOARD: bool = true;
+
 impl Default for Settings {
     fn default() -> Self {
         Self {
-            enhanced_clipboard: true,
+            enhanced_clipboard: DEFAULT_ENHANCED_CLIPBOARD,
             display_errors: true,
             start_with_os: false,
 
